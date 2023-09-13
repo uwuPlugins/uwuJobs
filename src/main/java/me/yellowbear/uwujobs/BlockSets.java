ebba4e0f505cb8ade7f6150e9af0c6558624afa6
Mapetr <info@mapetr.moe>
2023-09-13 09:54:15 +0200

    Move blocks to config.yml (#7)
    * change yaml to sqlite db
    
    * change yaml to sqlite db
    
    * Moved blocks to config.yml

--- src/main/java/me/yellowbear/uwujobs/BlockSets.java
+++ src/main/java/me/yellowbear/uwujobs/BlockSets.java
@@ -1,6 +1,9 @@
 package me.yellowbear.uwujobs;
 
 import org.bukkit.Material;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -8,11 +11,35 @@ import java.util.Map;
 import java.util.Set;
 
 public class BlockSets {
-    public Map<Material, Integer> minerBlocks = new HashMap<Material, Integer>();
+    public Map<Material, Integer> minerBlocks = new HashMap<>();
     public Map<Material, Integer> lumberBlocks = new HashMap<>();
     public Map<Material, Integer> farmerBlocks = new HashMap<>();
-    public BlockSets() throws NullPointerException {
-        minerBlocks.put(Material.DIAMOND_ORE, 5);
-        minerBlocks.put(Material.STONE, 1);
+
+    public BlockSets(FileConfiguration file) {
+        loadConfig(file);
+    }
+
+    private void loadConfig(FileConfiguration file) {
+        ConfigurationSection miner = file.getConfigurationSection("miner");
+        ConfigurationSection lumber = file.getConfigurationSection("lumber");
+        ConfigurationSection farmer = file.getConfigurationSection("farmer");
+
+        if (miner != null) {
+            for (String key : miner.getKeys(true)) {
+                minerBlocks.put(Material.getMaterial(key), file.getInt("miner." + key));
+            }
+        }
+
+        if (lumber != null) {
+            for (String key : lumber.getKeys(true)) {
+                lumberBlocks.put(Material.getMaterial(key), file.getInt("lumber." + key));
+            }
+        }
+
+        if (farmer != null) {
+            for (String key : farmer.getKeys(true)) {
+                farmerBlocks.put(Material.getMaterial(key), file.getInt("farmer." + key));
+            }
+        }
     }
 }
