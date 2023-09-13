3b5b68a358360b7959243dc707f4f68df217af54
MartinRostejnsky <martin.rostejnsky@email.cz>
2023-09-13 19:31:51 +0200

    begin work on leaderboards

--- src/main/java/me/yellowbear/uwujobs/CommandExecutor.java
+++ src/main/java/me/yellowbear/uwujobs/CommandExecutor.java
@@ -24,35 +24,56 @@ public class CommandExecutor {
         commandMap.register("jobs", new Command("jobs") {
             @Override
             public boolean execute(@NotNull CommandSender commandSender, @NotNull String s, @NotNull String[] args) {
-                if (args.length == 0 && commandSender instanceof Player) {
+                if (commandSender instanceof Player) {
                     Player player = (Player) commandSender;
-                    commandSender.sendMessage(
-                            ChatColor.AQUA + "Running "
-                            + ChatColor.LIGHT_PURPLE + "uwuJobs"
-                            + ChatColor.AQUA + " version "
-                            + ChatColor.LIGHT_PURPLE + "1.0"
-                            + ChatColor.AQUA + " by "
-                            + ChatColor.LIGHT_PURPLE + "yellowbear"
-                            + ChatColor.AQUA + " & "
-                            + ChatColor.LIGHT_PURPLE + "mapetr"
-                            + "\n"
-                    );
+                    if (args.length == 0) {
+                        commandSender.sendMessage(
+                                ChatColor.AQUA + "Running "
+                                + ChatColor.LIGHT_PURPLE + "uwuJobs"
+                                + ChatColor.AQUA + " version "
+                                + ChatColor.LIGHT_PURPLE + "1.0"
+                                + ChatColor.AQUA + " by "
+                                + ChatColor.LIGHT_PURPLE + "yellowbear"
+                                + ChatColor.AQUA + " & "
+                                + ChatColor.LIGHT_PURPLE + "mapetr"
+                                + "\n"
+                        );
 
 
-                    try (Connection conn = DatabaseConnector.connect()) {
-                        Statement statement = conn.createStatement();
-                        for (Jobs job : Jobs.values()) {
-                            ResultSet rs = statement.executeQuery(String.format("SELECT level FROM %s WHERE id = '%s'", job.name().toLowerCase(), player.getUniqueId().toString()));
-                            commandSender.sendMessage(ChatColor.AQUA + "You have level "
-                                    + ChatColor.LIGHT_PURPLE + rs.getInt("level")
-                                    + ChatColor.AQUA + " in profession "
-                                    + ChatColor.LIGHT_PURPLE + job.name()
-                            );
+                        try (Connection conn = DatabaseConnector.connect()) {
+                            Statement statement = conn.createStatement();
+                            for (Jobs job : Jobs.values()) {
+                                ResultSet rs = statement.executeQuery(String.format("SELECT level FROM %s WHERE id = '%s'", job.name().toLowerCase(), player.getUniqueId().toString()));
+                                commandSender.sendMessage(ChatColor.AQUA + "You have level "
+                                        + ChatColor.LIGHT_PURPLE + rs.getInt("level")
+                                        + ChatColor.AQUA + " in profession "
+                                        + ChatColor.LIGHT_PURPLE + job.name()
+                                );
+                            }
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
                         }
-                    } catch (SQLException e) {
-                        throw new RuntimeException(e);
+                        //zapomnel jsem jak ziskat informace o pluginu, mohlo by se nekdy dodelat
+                    } else {
+                        if (args[0].equalsIgnoreCase("top")) {
+                            try (Connection conn = DatabaseConnector.connect()) {
+                                Statement statement = conn.createStatement();
+                                ResultSet rs = statement.executeQuery(String.format("SELECT id, level FROM miner ORDER BY level DESC LIMIT 5;"));
+                                int i = 1;
+                                while (rs.next()) {
+                                    commandSender.sendMessage(
+                                                        ChatColor.AQUA + String.valueOf(i) + ". "
+                                                        + ChatColor.LIGHT_PURPLE + rs.getString("id")
+                                                                + ChatColor.AQUA + ", level "
+                                                        + ChatColor.LIGHT_PURPLE + String.valueOf(rs.getInt("level"))
+                                    );
+                                    i++;
+                                }
+                            } catch (SQLException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
                     }
-                    //zapomnel jsem jak ziskat informace o pluginu, mohlo by se nekdy dodelat
                 }
                 return true;
             }
