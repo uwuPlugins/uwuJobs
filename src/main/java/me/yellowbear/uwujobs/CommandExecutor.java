fdc4034a1b52317fd9544b0097febcec97ccfd5a
MartinRostejnsky <martin.rostejnsky@email.cz>
2023-09-13 08:45:58 +0200

    first working command & added mapetr to authors

--- src/main/java/me/yellowbear/uwujobs/CommandExecutor.java
+++ src/main/java/me/yellowbear/uwujobs/CommandExecutor.java
@@ -1,6 +1,7 @@
 package me.yellowbear.uwujobs;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
@@ -15,10 +16,11 @@ public class CommandExecutor {
         bukkitCommandMap.setAccessible(true);
         CommandMap commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
 
-        commandMap.register("test", new Command("test") {
+        commandMap.register("jobs", new Command("jobs") {
             @Override
-            public boolean execute(@NotNull CommandSender commandSender, @NotNull String s, @NotNull String[] strings) {
-                commandSender.sendMessage("kys");
+            public boolean execute(@NotNull CommandSender commandSender, @NotNull String s, @NotNull String[] args) {
+                commandSender.sendMessage(ChatColor.LIGHT_PURPLE.toString() + "uwuJobs v1.0" + ChatColor.AQUA + " by " + ChatColor.LIGHT_PURPLE + "yellowbear" + ChatColor.AQUA + " & " + ChatColor.LIGHT_PURPLE + "mapetr");
+                //zapomnel jsem jak ziskat informace o pluginu, mohlo by se nekdy dodelat
                 return true;
             }
         });
