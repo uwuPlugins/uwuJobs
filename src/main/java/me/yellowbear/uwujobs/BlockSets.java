289ef1271a6ab3d6933480658d24c427c6bda52c
Martin Roštejnský <91248036+MartinRostejnsky@users.noreply.github.com>
2023-09-16 09:45:01 +0200

    refactor/scalability (#24)
    * rework block maps to general job maps mapping jobs to block maps
    
    * rework handleBlockMined method to loop through all jobs instead of just hardcoding them
    
    * remove some unused leftover lines
    
    * loadConfig method rework

--- src/main/java/me/yellowbear/uwujobs/BlockSets.java
+++ src/main/java/me/yellowbear/uwujobs/BlockSets.java
@@ -8,34 +8,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BlockSets {
-    public Map<Material, Integer> minerBlocks = new HashMap<>();
-    public Map<Material, Integer> lumberBlocks = new HashMap<>();
-    public Map<Material, Integer> farmerBlocks = new HashMap<>();
-
+    public Map<Jobs, Map<Material, Integer>> jobsMap = new HashMap<>();
     public BlockSets(FileConfiguration file) {
         loadConfig(file);
     }
-
     private void loadConfig(FileConfiguration file) {
-        ConfigurationSection miner = file.getConfigurationSection(Jobs.MINER.name());
-        ConfigurationSection lumber = file.getConfigurationSection(Jobs.LUMBER.name());
-        ConfigurationSection farmer = file.getConfigurationSection(Jobs.FARMER.name());
-
-        if (miner != null) {
-            for (String key : miner.getKeys(true)) {
-                minerBlocks.put(Material.getMaterial(key), file.getInt(Jobs.MINER.name()+"." + key));
-            }
-        }
-
-        if (lumber != null) {
-            for (String key : lumber.getKeys(true)) {
-                lumberBlocks.put(Material.getMaterial(key), file.getInt(Jobs.LUMBER.name()+"." + key));
-            }
-        }
-
-        if (farmer != null) {
-            for (String key : farmer.getKeys(true)) {
-                farmerBlocks.put(Material.getMaterial(key), file.getInt(Jobs.FARMER.name()+"." + key));
+        for (Jobs job : Jobs.values()) {
+            ConfigurationSection section = file.getConfigurationSection(job.name());
+            Map<Material, Integer> tempMap = new HashMap<>();
+            if (section != null) {
+                for (String key : section.getKeys(true)) {
+                    tempMap.put(Material.getMaterial(key), file.getInt(job.name()+"." + key));
+                }
+                jobsMap.put(job,tempMap);
             }
         }
     }
--- src/main/java/me/yellowbear/uwujobs/Job.java
+++ src/main/java/me/yellowbear/uwujobs/Job.java
@@ -1,6 +1,5 @@
 package me.yellowbear.uwujobs;
 
-import org.bukkit.Material;
 import org.bukkit.block.data.Ageable; //Nezamenovat s Entity.Ageable!!!
 import org.bukkit.event.block.BlockBreakEvent;
 
@@ -10,21 +9,16 @@ import static me.yellowbear.uwujobs.Level.awardXp;
 
 public class Job {
     public static void handleBlockMined(BlockBreakEvent event, BlockSets blockSets) throws IOException {
-        if (blockSets.minerBlocks.get(event.getBlock().getType()) != null) {
-            awardXp(event.getPlayer(), blockSets.minerBlocks.get(event.getBlock().getType()), Jobs.MINER);
+        Ageable ageable;
+        try {
+            ageable = (Ageable) event.getBlock().getBlockData();
+        } catch (Exception ex) { ageable = null; }
+        if (!(ageable == null || ageable.getAge() == 7)) {
+            return;
         }
-        if (blockSets.lumberBlocks.get(event.getBlock().getType()) != null) {
-            awardXp(event.getPlayer(), blockSets.lumberBlocks.get(event.getBlock().getType()), Jobs.LUMBER);
-        }
-        if (blockSets.farmerBlocks.get(event.getBlock().getType()) != null) {
-            try {
-                Ageable ageable = (Ageable) event.getBlock().getBlockData();
-                System.err.println(ageable.getAge());
-                if (ageable.getAge() == 7) {
-                    awardXp(event.getPlayer(), blockSets.farmerBlocks.get(event.getBlock().getType()), Jobs.FARMER);
-                }
-            } catch (Exception ex) {
-                throw new RuntimeException(ex);
+        for (Jobs job : Jobs.values()) {
+            if (blockSets.jobsMap.get(job).get(event.getBlock().getType()) != null) {
+                awardXp(event.getPlayer(), blockSets.jobsMap.get(job).get(event.getBlock().getType()), job);
             }
         }
     }
