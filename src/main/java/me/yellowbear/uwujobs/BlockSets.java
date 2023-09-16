fa152ab6eb98fdaaa08dfc81fb95068d7ba5e24a
MartinRostejnsky <martin.rostejnsky@email.cz>
2023-09-16 09:31:17 +0200

    loadConfig method rework

--- src/main/java/me/yellowbear/uwujobs/BlockSets.java
+++ src/main/java/me/yellowbear/uwujobs/BlockSets.java
@@ -9,40 +9,19 @@ import java.util.Map;
 
 public class BlockSets {
     public Map<Jobs, Map<Material, Integer>> jobsMap = new HashMap<>();
-    private Map<Material, Integer> minerBlocks = new HashMap<>();
-    private Map<Material, Integer> lumberBlocks = new HashMap<>();
-    private Map<Material, Integer> farmerBlocks = new HashMap<>();
-
     public BlockSets(FileConfiguration file) {
         loadConfig(file);
     }
-
     private void loadConfig(FileConfiguration file) {
-        ConfigurationSection miner = file.getConfigurationSection(Jobs.MINER.name());
-        ConfigurationSection lumber = file.getConfigurationSection(Jobs.LUMBER.name());
-        ConfigurationSection farmer = file.getConfigurationSection(Jobs.FARMER.name());
-
-        if (miner != null) {
-            for (String key : miner.getKeys(true)) {
-                minerBlocks.put(Material.getMaterial(key), file.getInt(Jobs.MINER.name()+"." + key));
+        for (Jobs job : Jobs.values()) {
+            ConfigurationSection section = file.getConfigurationSection(job.name());
+            Map<Material, Integer> tempMap = new HashMap<>();
+            if (section != null) {
+                for (String key : section.getKeys(true)) {
+                    tempMap.put(Material.getMaterial(key), file.getInt(job.name()+"." + key));
+                }
+                jobsMap.put(job,tempMap);
             }
         }
-
-        if (lumber != null) {
-            for (String key : lumber.getKeys(true)) {
-                lumberBlocks.put(Material.getMaterial(key), file.getInt(Jobs.LUMBER.name()+"." + key));
-            }
-        }
-
-        if (farmer != null) {
-            for (String key : farmer.getKeys(true)) {
-                farmerBlocks.put(Material.getMaterial(key), file.getInt(Jobs.FARMER.name()+"." + key));
-            }
-        }
-
-        jobsMap.put(Jobs.MINER, minerBlocks);
-        jobsMap.put(Jobs.LUMBER, lumberBlocks);
-        jobsMap.put(Jobs.FARMER, farmerBlocks);
-
     }
 }
