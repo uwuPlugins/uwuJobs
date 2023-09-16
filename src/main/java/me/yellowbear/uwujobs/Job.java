a2d5a77b9baf7750bdfda0b3541e2847c4b6f2e6
MartinRostejnsky <martin.rostejnsky@email.cz>
2023-09-16 09:19:53 +0200

    rework handleBlockMined method to loop through all jobs instead of just hardcoding them

--- src/main/java/me/yellowbear/uwujobs/Job.java
+++ src/main/java/me/yellowbear/uwujobs/Job.java
@@ -10,21 +10,16 @@ import static me.yellowbear.uwujobs.Level.awardXp;
 
 public class Job {
     public static void handleBlockMined(BlockBreakEvent event, BlockSets blockSets) throws IOException {
-        if (blockSets.jobsMap.get(Jobs.MINER).get(event.getBlock().getType()) != null) {
-            awardXp(event.getPlayer(), blockSets.jobsMap.get(Jobs.MINER).get(event.getBlock().getType()), Jobs.MINER);
+        Ageable ageable;
+        try {
+            ageable = (Ageable) event.getBlock().getBlockData();
+        } catch (Exception ex) { ageable = null; }
+        if (!(ageable == null || ageable.getAge() == 7)) {
+            return;
         }
-        if (blockSets.jobsMap.get(Jobs.LUMBER).get(event.getBlock().getType()) != null) {
-            awardXp(event.getPlayer(), blockSets.jobsMap.get(Jobs.LUMBER).get(event.getBlock().getType()), Jobs.LUMBER);
-        }
-        if (blockSets.jobsMap.get(Jobs.FARMER).get(event.getBlock().getType()) != null) {
-            try {
-                Ageable ageable = (Ageable) event.getBlock().getBlockData();
-                System.err.println(ageable.getAge());
-                if (ageable.getAge() == 7) {
-                    awardXp(event.getPlayer(), blockSets.jobsMap.get(Jobs.FARMER).get(event.getBlock().getType()), Jobs.FARMER);
-                }
-            } catch (Exception ex) {
-                throw new RuntimeException(ex);
+        for (Jobs job : Jobs.values()) {
+            if (blockSets.jobsMap.get(job).get(event.getBlock().getType()) != null) {
+                awardXp(event.getPlayer(), blockSets.jobsMap.get(job).get(event.getBlock().getType()), job);
             }
         }
     }
