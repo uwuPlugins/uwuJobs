1680abc523868943f2ceb9ccc731be4485b4d1ee
MartinRostejnsky <martin.rostejnsky@email.cz>
2024-06-22 20:54:45 +0200

    comments for config.kt

--- src/main/java/me/yellowbear/uwujobs/Config.kt
+++ src/main/java/me/yellowbear/uwujobs/Config.kt
@@ -5,9 +5,22 @@ import me.yellowbear.uwujobs.interfaces.Config
 import me.yellowbear.uwujobs.jobs.Job
 import java.io.File
 
+/**
+ * Global plugin config object.
+ *
+ * @property jobs Array of all the configured jobs.
+ * @property loadJobs Loads the job data from a config file.
+ * @property getJob Returns a job with given name (if exists).
+ */
 object Config {
+    /**
+     * Array of all the configured jobs.
+     */
     var jobs: Array<Job> = emptyArray()
 
+    /**
+     * Loads the job data from a config file.
+     */
     fun loadJobs() {
         UwuJobs().saveResource("jobs.yml", false)
 
@@ -17,6 +30,12 @@ object Config {
         UwuJobs().logger.info("Loaded ${jobs.size} jobs")
     }
 
+    /**
+     * Returns a job with given name (if exists).
+     *
+     * @param name Target job name.
+     * @return A job with the specified target name.
+     */
     fun getJob(name: String): Job? {
         return jobs.find { it.name == name || it.name.lowercase() == name }
     }
