b1d8a43ca6219932c332f49800170227e9d678db
MartinRostejnsky <martin.rostejnsky@email.cz>
2024-06-22 20:29:22 +0200

    summaries for the job data objects

--- src/main/java/me/yellowbear/uwujobs/jobs/Job.kt
+++ src/main/java/me/yellowbear/uwujobs/jobs/Job.kt
@@ -2,6 +2,15 @@ package me.yellowbear.uwujobs.jobs
 
 import kotlinx.serialization.Serializable
 
+/**
+ * An serializable uwuJobs reward object.
+ *
+ * @property brokenBlocks A list of blocks that give a player a reward for breaking them.
+ * @property placedBlocks A list of blocks that give a player a reward for placing them.
+ * @property killedEntities A list of entities that give a player a reward for killing them.
+ * @property fertilizedBlocks A list of blocks that give a player a reward for fertilizing them.
+ * @property amount Amount of XP the player is to receive.
+ */
 @Serializable
 data class Reward(
     val brokenBlocks: List<String>? = null,
@@ -11,6 +20,12 @@ data class Reward(
     val amount: Int
 )
 
+/**
+ * An serializable uwuJobs job object.
+ *
+ * @property name Display name of the job.
+ * @property rewards A list of rewards that apply to the job.
+ */
 @Serializable
 data class Job(
     val name: String,
